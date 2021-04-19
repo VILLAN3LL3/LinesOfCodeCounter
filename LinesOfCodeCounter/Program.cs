ca59564a47df40a36b961f147c1c93ee22ec8080
Mira Manger <mira.manger@haufe-lexware.com>
2021-04-19 07:28:02 +0200

    Projektdateien hinzufügen.

---
+++ LinesOfCodeCounter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinesOfCodeCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
