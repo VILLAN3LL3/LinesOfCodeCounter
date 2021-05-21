6e0601359f1683405390e4d2b5b16dbea2ad7ee8
Mira Manger <mira.manger@haufe-lexware.com>
2021-05-21 09:40:33 +0200

    removed unnecessary check

--- LinesOfCodeCounter/Domain/LineCounter.cs
+++ LinesOfCodeCounter/Domain/LineCounter.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 
 namespace LinesOfCodeCounter.Domain
 {
@@ -10,11 +9,9 @@ namespace LinesOfCodeCounter.Domain
         public int CountLinesOfCode(string[] lines) => lines
             .Count(line =>
                    !IsEmptyLine(line)
-                && !IsComment(line)
-                && !IsLineBreak(line));
+                && !IsComment(line));
 
         private bool IsEmptyLine(string line) => string.IsNullOrWhiteSpace(line);
         private bool IsComment(string line) => line.TrimStart().StartsWith("//");
-        private bool IsLineBreak(string line) => Environment.NewLine.Equals(line);
     }
 }
